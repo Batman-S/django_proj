9dd2aa37ff8edd3c89c018de2c07ecea96ffa6b6
Batman-S <83809552+Batman-S@users.noreply.github.com>
2021-12-05 21:02:12 +0900

    completed adding third party templates for user account settings and login/register pages

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def loginPage(request):
@@ -179,3 +179,18 @@ def deleteMessage(request, pk):
         return HttpResponse("You do you not have permission")
 
     return render(request, "base/delete.html", {"obj": message})
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    context = {"form": form}
+    
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    
+    return render(request, 'base/update_user.html', context)
+    
